9b81602f4a64e962dbcf6c2d9bbc09f6937a6b37
Ayesha Quazi <aayeshaquazi04@gmail.com>
2024-11-21 16:59:59 +0530

    Day 6

---
+++ Day07.cpp
@@ -0,0 +1,22 @@
+// Stock Buy and Sell – Multiple Transaction Allowed
+
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maximumProfit(vector<int>& prices) {
+        int profit = 0; // Initialize profit
+        int n = prices.size(); // Size of the prices array
+
+        for (int i = 0; i < n - 1; i++) {
+            // If the next day's price is higher, add the difference to profit
+            if (prices[i + 1] > prices[i]) {
+                profit += prices[i + 1] - prices[i];
+            }
+        }
+
+        return profit; // Return the total profit
+    }
+};
