c1b70e8dca7622e7b2f8fd8d897eeb108d131ef9
Ayesha Quazi <aayeshaquazi04@gmail.com>
2024-12-19 19:38:41 +0530

    code updated!

--- Day35.cpp
+++ Day35.cpp
@@ -1,1 +1,21 @@
 // Kth Missing Positive Number in a Sorted Array
+
+class Solution {
+  public:
+    int kthMissing(vector<int> &arr, int k) {
+        // Your code goes here
+        int n=arr.size();
+        int hi=n-1, lo=0, res=k+n;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(arr[mid]>mid+k){
+                res=mid+k;
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return res;
+    }
+};
