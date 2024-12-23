ab85defdf26fca12da0056209fdc475f50a5fde7
Ayesha Quazi <aayeshaquazi04@gmail.com>
2024-12-23 19:38:03 +0530

    code added

--- Day38.cpp
+++ Day38.cpp
@@ -1,2 +1,18 @@
 // Search in a Row-Column sorted matrix
 
+
+class Solution {
+  public:
+    bool matSearch(vector<vector<int>> &mat, int x) {
+        //Code Here
+        int n=mat.size();
+        int m=mat[0].size();
+        int i=0,j=m-1;
+        while(i<n && j>=0){
+            if(mat[i][j]==x) return true;
+            if(mat[i][j]>x) j--;
+            else i++;
+        }
+        return false;
+    }
+};
