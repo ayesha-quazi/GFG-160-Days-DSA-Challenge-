5a0c67091a57b4731b42e81ddce4137dfca435d7
Ayesha Quazi <aayeshaquazi04@gmail.com>
2024-12-24 11:55:06 +0530

    Day 40

--- Day40.cpp
+++ Day40.cpp
@@ -4,6 +4,7 @@ class Solution {
   public:
     // Function to search a given number in row-column sorted matrix.
     bool searchMatrix(vector<vector<int>> &mat, int x) {
+
         // code here
         
         int n=mat.size();
@@ -19,5 +20,3 @@ class Solution {
         return false;
     }
 };
-
-
