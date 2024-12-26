ceea080cf27e1295180fa2fac15deaa664f8f7cd
Ayesha Quazi <aayeshaquazi04@gmail.com>
2024-12-26 18:22:09 +0530

    code added

--- Day42.cpp
+++ Day42.cpp
@@ -1,1 +1,16 @@
 // Two Sum - Pair with Given Sum
+
+
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        //Code Here
+        unordered_map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            int diff=target-arr[i];
+            if(mp.find(diff)!=mp.end()) return true;
+            mp[arr[i]]++;
+        }
+        return false;
+    }
+};
