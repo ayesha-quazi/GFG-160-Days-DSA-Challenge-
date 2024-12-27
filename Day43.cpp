b17b0cc8a86db1168af06d5c430e76f1e391bd1d
Ayesha Quazi <aayeshaquazi04@gmail.com>
2024-12-27 15:57:26 +0530

    code added

--- Day43.cpp
+++ Day43.cpp
@@ -1,1 +1,17 @@
 // Count pairs with given sum
+
+
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        // Code here
+        unordered_map<int,int>mp;
+        int count=0;
+        for(int i=0;i<arr.size();i++){
+            int second=target-arr[i];
+            count+=mp[second];
+            mp[arr[i]]++;
+        }
+        return count;
+    }
+};
