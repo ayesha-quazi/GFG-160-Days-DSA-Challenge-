6ac6657fb81b401ab07078fc7b6d16126ad8b1b8
Ayesha Quazi <aayeshaquazi04@gmail.com>
2025-01-02 21:32:34 +0530

    code added

---
+++ Day49.cpp
@@ -0,0 +1,19 @@
+// 
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int countSubarrays(vector<int> &arr, int k) {
+        // code here
+        unordered_map<int,int>mp;
+        int sum=0,count=0;
+        for(int i=0;i<arr.size();i++){
+            sum+=arr[i];
+            if(sum==k) count++;
+            if(mp.find(sum-k)!=mp.end()) count+=mp[sum-k];
+            mp[sum]++;
+        }
+        return count;
+    }
+};
