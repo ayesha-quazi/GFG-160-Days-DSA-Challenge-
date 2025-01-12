3e315c04c6f32f1edd2d66105d594b702e6152ac
Ayesha Quazi <aayeshaquazi04@gmail.com>
2025-01-12 12:00:09 +0530

    code added

--- Day59.cpp
+++ Day59.cpp
@@ -1,1 +1,26 @@
 // Trapping Rain Water
+
+
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        // code here
+        
+        int totalWater = 0;
+        int lmax = 0, rmax = 0;
+        int i = 0, j = arr.size()-1;
+        while(i<j){
+            lmax=max(lmax,arr[i]);
+            rmax=max(rmax,arr[j]);
+            if(arr[i]<=arr[j]){
+                totalWater += lmax-arr[i];
+                i++;
+            }
+            else{
+                totalWater+=rmax-arr[j];
+                j--;
+            }
+        }
+        return totalWater;
+    }
+};
